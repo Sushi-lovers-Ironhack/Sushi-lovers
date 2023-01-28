578210f31214855243f95724a1865f2698a74f0d
Alberto Valenzuela <avalen28@gmail.com>
2023-01-28 13:49:45 +0100

    adds cookie name

--- app.js
+++ app.js
@@ -1,70 +1,70 @@
-require('dotenv').config();
-require('./db');
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+require("dotenv").config();
+require("./db");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 // Routers require
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
 
 const app = express();
 
 // cookies and loggers
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // For deployment
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 app.use(
   session({
-    name: 'project2-cookie',
+    name: "sushiLovers-cookie",
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: false,
     cookie: {
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-      secure: process.env.NODE_ENV === 'production',
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
       httpOnly: true,
-      maxAge: 2592000000 // 30 days in milliseconds
+      maxAge: 2592000000, // 30 days in milliseconds
     },
     store: MongoStore.create({
-      mongoUrl: process.env.MONGO_URL
-    })
-  }) 
-)
+      mongoUrl: process.env.MONGO_URL,
+    }),
+  })
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 // routes intro
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
+app.use("/", indexRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   if (err.status === 404) {
-    res.render('404', { path: req.url });
+    res.render("404", { path: req.url });
   } else {
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error");
   }
 });
 
