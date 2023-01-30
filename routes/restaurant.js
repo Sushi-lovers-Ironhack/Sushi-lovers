e08f4748ab48ab9f42248c3ea0b06686fb2a0990
Gerard Guasch <haldawe@hotmail.com>
2023-01-30 18:05:57 +0100

    created restaurant profile view and route

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,6 +1,12 @@
 const Restaurant = require('../models/Restaurant');
 const router = require('express').Router();
 
-
+// @desc    Sends restaurant profile info
+// @route   POST /restaurant/profile
+// @access  Restaurants
+router.get('/profile', (req, res, next) => {
+    const restaurant = req.session.currentUser;
+    res.render("./restaurant/profile", restaurant);
+});
 
 module.exports = router;
