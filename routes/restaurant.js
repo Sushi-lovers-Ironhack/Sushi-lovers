0dedae9e19876a60378b87e3c9eb9ab61db469f5
Gerard Guasch <haldawe@hotmail.com>
2023-01-30 18:13:48 +0100

    created delete profile route

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,12 +1,27 @@
+const { findByIdAndDelete } = require('../models/Restaurant');
 const Restaurant = require('../models/Restaurant');
 const router = require('express').Router();
 
 // @desc    Sends restaurant profile info
-// @route   POST /restaurant/profile
+// @route   GET /restaurant/profile
 // @access  Restaurants
 router.get('/profile', (req, res, next) => {
     const restaurant = req.session.currentUser;
     res.render("./restaurant/profile", restaurant);
+});
+
+// @desc    Deletes restaurant and items from it from the database
+// @route   GET /restaurant/profile/delete
+// @access  Restaurants
+router.get('/profile/delete', async (req, res, next) => {
+    const restaurantId = req.session.currentUser._id;
+    // To do: search all menu items from this restaurant and delete them
+    try {
+        await Restaurant.findByIdAndDelete(restaurantId);
+        res.redirect("/auth/login");
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
