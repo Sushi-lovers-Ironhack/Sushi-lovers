238433598127f618d770de62deaf41f55c24f794
Alberto Valenzuela <avalen28@gmail.com>
2023-01-30 19:49:13 +0100

    adds User Model with some design questions

--- models/User.js
+++ models/User.js
@@ -1,31 +1,57 @@
-const { Schema, model } = require('mongoose');
- 
+const { Schema, model } = require("mongoose");
+
 const userSchema = new Schema(
   // Add whichever fields you need for your app
   {
     username: {
       type: String,
       trim: true,
-      required: [true, 'Username is required.'],
-      unique: true
+      required: [true, "Username is required."],
+      unique: true,
+    },
+    surname: {
+      //Alberto: ésto podríamos no requerirlo y que luego el user lo añada?
+      type: String,
+      trim: true,
+      required: [true, "Surname is required."],
+      unique: true,
+    },
+    direction: {
+      //Alberto: ésto podríamos no requerirlo y que luego el user lo añada?
+      type: String,
+      trim: true,
+      required: [true, "Direction is required."],
+      unique: true,
     },
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, "Email is required."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
+    },
+    phoneNumber: {
+      //Alberto: ésto podríamos no requerirlo y que luego el user lo añada?
+      type: Number,
+      trim: true,
+      required: [true, "Phone number is required."],
+      unique: true,
     },
     hashedPassword: {
       type: String,
-      required: [true, 'Password is required.']
-    }
+      required: [true, "Password is required."],
+    },
+    paymentCard: {
+      //Alberto: ésto podríamos no requerirlo y que luego el user lo añada?
+      type: Number,
+      required: [true, "Payment card is required."],
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
- 
-const User = model('User', userSchema);
+
+const User = model("User", userSchema);
 
 module.exports = User;
