92dbecca044a3936f5f1875a878c4302b8e827de
Alberto Valenzuela <avalen28@gmail.com>
2023-01-31 19:00:32 +0100

    reposition user signup get on .js

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
-const Restaurant = require('../models/Restaurant');
-const bcrypt = require('bcrypt');
+const User = require("../models/User");
+const Restaurant = require("../models/Restaurant");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
-
-// @desc    Displays form view to sign up
-// @route   GET /auth/signup
-// @access  Public
-router.get('/signup', async (req, res, next) => {
-  res.render('auth/signup');
-})
 
 // @desc    Displays form view  for restaurants to sign up
 // @route   GET /restaurant/signup
@@ -19,92 +12,135 @@ router.get("/restaurant/signup", (req, res, next) => {
   res.render("auth/restaurantSignup");
 });
 
-
 // @desc    Sends restaurant auth data to database to create a new user
 // @route   POST /restaurant/signup
 // @access  Public
 router.post("/restaurant/signup", async (req, res, next) => {
-  const { name, email, password1, password2, direction, phoneNumber, description } = req.body;
-  if (!name || !email || !password1 || !password2 || !direction || !phoneNumber || !description) {
-      res.render("auth/restaurantSignup", {error: 'Must fill all fields'});
-      return;
-  };
-  const regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
+  const {
+    name,
+    email,
+    password1,
+    password2,
+    direction,
+    phoneNumber,
+    description,
+  } = req.body;
+  if (
+    !name ||
+    !email ||
+    !password1 ||
+    !password2 ||
+    !direction ||
+    !phoneNumber ||
+    !description
+  ) {
+    res.render("auth/restaurantSignup", { error: "Must fill all fields" });
+    return;
+  }
+  const regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
   if (!regexEmail.test(email)) {
-      res.render("auth/restaurantSignup", {error: 'Must provide a valid email'});
-      return;
-  };
-  const regexPassword = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
+    res.render("auth/restaurantSignup", {
+      error: "Must provide a valid email",
+    });
+    return;
+  }
+  const regexPassword =
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
   if (!regexPassword.test(password1)) {
-      res.render("auth/restaurantSignup", {error: 'Password must have at least 8 characters and contain one uppercase and lowercase letter, a special character and a number'});
-      return;
-  };
+    res.render("auth/restaurantSignup", {
+      error:
+        "Password must have at least 8 characters and contain one uppercase and lowercase letter, a special character and a number",
+    });
+    return;
+  }
   if (!regexPassword.test(password2)) {
-    res.render("auth/restaurantSignup", {error: 'Doublecheck the password on both fields'});
+    res.render("auth/restaurantSignup", {
+      error: "Doublecheck the password on both fields",
+    });
     return;
-  };
-  const regexPhone = /^\+?(6\d{2}|7[1-9]\d{1})\d{6}$/
+  }
+  const regexPhone = /^\+?(6\d{2}|7[1-9]\d{1})\d{6}$/;
   if (!regexPhone.test(phoneNumber)) {
-    res.render("auth/restaurantSignup", {error: 'Correct phone number is required'});
+    res.render("auth/restaurantSignup", {
+      error: "Correct phone number is required",
+    });
     return;
-  };
+  }
   if (!password1 === password2) {
-    res.render("auth/restaurantSignup", {error: 'Doublecheck the password on both fields'});
+    res.render("auth/restaurantSignup", {
+      error: "Doublecheck the password on both fields",
+    });
     return;
-  };
+  }
   try {
-      const foundUser = await Restaurant.findOne({ name });
-      if (foundUser) {
-          res.render("auth/restaurantSignup", {error: 'Restaurant name alreday in use'});
-          return;
-      };
-      const foundEmailRestaurant = await Restaurant.findOne({ email })
-      const foundEmailUser = await User.findOne({ email });
-      if (foundEmailUser || foundEmailRestaurant) {
-          res.render("auth/restaurantSignup", {error: 'Email already in use'});
-          return;
-      }
-      const salt = await bcrypt.genSalt(saltRounds);
-      const hashedPassword = await bcrypt.hash(password1, salt);
-      await Restaurant.create({ name, email, hashedPassword, direction, phoneNumber, description });
-      res.redirect('/restaurant/profile');
+    const foundUser = await Restaurant.findOne({ name });
+    if (foundUser) {
+      res.render("auth/restaurantSignup", {
+        error: "Restaurant name alreday in use",
+      });
+      return;
+    }
+    const foundEmailRestaurant = await Restaurant.findOne({ email });
+    const foundEmailUser = await User.findOne({ email });
+    if (foundEmailUser || foundEmailRestaurant) {
+      res.render("auth/restaurantSignup", { error: "Email already in use" });
+      return;
+    }
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password1, salt);
+    await Restaurant.create({
+      name,
+      email,
+      hashedPassword,
+      direction,
+      phoneNumber,
+      description,
+    });
+    res.redirect("/restaurant/profile");
   } catch (error) {
-      next(error);
+    next(error);
   }
 });
 
 // @desc    Displays form view to log in
 // @route   GET /auth/login
 // @access  Public
-router.get('/login', async (req, res, next) => {
-  res.render('auth/login');
-})
+router.get("/login", async (req, res, next) => {
+  res.render("auth/login");
+});
+
+// @desc    Displays form view to sign up
+// @route   GET /auth/signup
+// @access  Public
+router.get("/signup", async (req, res, next) => {
+  res.render("auth/signup");
+});
 
 // @desc    Sends user auth data to database to create a new user
 // @route   POST /auth/signup
 // @access  Public
-router.post('/signup', async (req, res, next) => {
+router.post("/signup", async (req, res, next) => {
   const { email, password, username } = req.body;
   // ⚠️ Add validations!
   try {
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(password, salt);
     const user = await User.create({ username, email, hashedPassword });
-    res.redirect('/user/profile')
+    res.redirect("/user/profile");
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
 // @desc    Sends user auth data to database to authenticate user
 // @route   POST /auth/login
 // @access  Public
-router.post('/login', async (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    res.render("users/login", {error: 'Must fill all fields'});
+    res.render("users/login", { error: "Must fill all fields" });
     return;
-  };
+  }
   try {
     const user = await User.findOne({ email: email });
     if (user) {
@@ -113,36 +149,36 @@ router.post('/login', async (req, res, next) => {
         req.session.currentUser = user;
         // To do: res.redirect('/home');
       } else {
-        res.render('auth/login', { error: "Unable to authenticate user" });
+        res.render("auth/login", { error: "Unable to authenticate user" });
       }
     }
-    const restaurant = await Restaurant.findOne({ email: email});
+    const restaurant = await Restaurant.findOne({ email: email });
     if (restaurant) {
       const match = await bcrypt.compare(password, restaurant.hashedPassword);
       if (match) {
         req.session.currentUser = restaurant;
         // To do: res.redirect('/restaurant');
       } else {
-        res.render('auth/login', { error: "Unable to authenticate user" });
+        res.render("auth/login", { error: "Unable to authenticate user" });
       }
-    };
-    res.render('auth/login', { error: "Unable to authenticate user"});
+    }
+    res.render("auth/login", { error: "Unable to authenticate user" });
   } catch (error) {
     next(error);
   }
-})
+});
 
 // @desc    Destroy user session and log out
 // @route   POST /auth/logout
-// @access  Private 
-router.post('/logout', (req, res, next) => {
+// @access  Private
+router.post("/logout", (req, res, next) => {
   req.session.destroy((err) => {
     if (err) {
-      next(err)
+      next(err);
     } else {
-      res.redirect('/auth/login');
+      res.redirect("/auth/login");
     }
   });
-})
+});
 
 module.exports = router;
