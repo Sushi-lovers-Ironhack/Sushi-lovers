033fa56083dcf3ad8948817236f2f07c22ecaf83
Gerard Guasch <haldawe@hotmail.com>
2023-02-04 15:47:41 +0100

    fixed logout error

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,6 @@ const { isUser, isLoggedIn } = require("../middlewares");
 // @desc    Sends User profile info
 // @route   GET /user/profile
 // @access  User
-
 router.get("/profile", isLoggedIn, isUser, (req, res, next) => {
   const user = req.session.currentUser;
   res.render("user/profile", user);
@@ -16,7 +15,6 @@ router.get("/profile", isLoggedIn, isUser, (req, res, next) => {
 // @desc    Deletes user and items from it from the database
 // @route   GET /user/profile/delete
 // @access  User
-
 router.get("/profile/delete", isLoggedIn, isUser, async (req, res, next) => {
   const userId = req.session.currentUser._id;
   // Backlog to do: search in all user the objectIds (Orders f.e.) and delete them
@@ -31,17 +29,16 @@ router.get("/profile/delete", isLoggedIn, isUser, async (req, res, next) => {
 // @desc    Sends user form with previous values for editing
 // @route   GET /user/profile/edit
 // @access  User
-
 router.get("/profile/edit", isLoggedIn, isUser, (req, res, next) => {
   const user = req.session.currentUser;
-  res.render("user/profileEdit", user);
+  res.render("user/profileEdit", {user, username: user});
 });
 
 // @desc    Sends User form with previous values for editing
 // @route   POST /user/profile/edit
 // @access  User
-
 router.post("/profile/edit", isLoggedIn, isUser, async (req, res, next) => {
+  const user = req.session.currentUser;
   const {
     username,
     surname,
@@ -62,13 +59,13 @@ router.post("/profile/edit", isLoggedIn, isUser, async (req, res, next) => {
     !phoneNumber ||
     !paymentCard
   ) {
-    res.render("user/profileEdit", { error: "Must fill all fields" });
+    res.render("user/profileEdit", { error: "Must fill all fields", user, username: user });
     return;
   }
   const regexEmail = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
   if (!regexEmail.test(email)) {
     res.render("user/profileEdit", {
-      error: "Must provide a valid email",
+      error: "Must provide a valid email",  user, username: user 
     });
     return;
   }
@@ -77,26 +74,26 @@ router.post("/profile/edit", isLoggedIn, isUser, async (req, res, next) => {
   if (!regexPassword.test(password1)) {
     res.render("./user/profileEdit", {
       error:
-        "Password must have at least 8 characters and contain one uppercase and lowercase letter, a special character and a number",
+        "Password must have at least 8 characters and contain one uppercase and lowercase letter, a special character and a number",  user, username: user 
     });
     return;
   }
   if (!regexPassword.test(password2)) {
     res.render("user/profileEdit", {
-      error: "Doublecheck the password on both fields",
+      error: "Doublecheck the password on both fields",  user, username: user 
     });
     return;
   }
   const regexPhone = /^\+?(6\d{2}|7[1-9]\d{1})\d{6}$/;
   if (!regexPhone.test(phoneNumber)) {
     res.render("user/profileEdit", {
-      error: "Correct phone number is required",
+      error: "Correct phone number is required",  user, username: user 
     });
     return;
   }
   if (!password1 === password2) {
     res.render("user/profileEdit", {
-      error: "Doublecheck the password on both fields",
+      error: "Doublecheck the password on both fields",  user, username: user 
     });
     return;
   }
