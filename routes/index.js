f5ef2223ea8dcd872526fdd85d7d74a3fe78b67b
Gerard Guasch <haldawe@hotmail.com>
2023-02-04 11:34:27 +0100

    home route and serach view completed

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
+const Restaurant = require("../models/Restaurant");
 
 // @desc    App home page
 // @route   GET /
 // @access  Public
-router.get("/", (req, res, next) => {
-  const user = req.session.currentUser;
-  res.render("home/home", user);
+router.get("/", async (req, res, next) => {
+  const username = req.session.currentUser;
+  const restaurantsDB = await Restaurant.find({}).limit(4);
+  res.render("home/home", {username, restaurantsDB});
 });
+
+// @desc    App home page
+// @route   GET /
+// @access  Public
+
 
 module.exports = router;
