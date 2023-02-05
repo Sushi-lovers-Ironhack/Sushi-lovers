1f68ca91236be1bf34ac6549c3a6ac381b74bf7b
Alberto Valenzuela <avalen28@gmail.com>
2023-02-05 21:09:50 +0100

    Adds logoUrl field to Restaurant model

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,46 +1,47 @@
-const { Schema, model } = require('mongoose');
- 
+const { Schema, model } = require("mongoose");
+
 const restaurantSchema = new Schema(
   // Add whichever fields you need for your app
   {
     name: {
       type: String,
       trim: true,
-      required: [true, 'Name is required.'],
-      unique: true
+      required: [true, "Name is required."],
+      unique: true,
     },
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, "Email is required."],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
     },
     hashedPassword: {
       type: String,
-      required: [true, 'Password is required.']
+      required: [true, "Password is required."],
     },
     direction: {
       type: String,
-      required: [true, 'Direction is required']
+      required: [true, "Direction is required"],
     },
     phoneNumber: {
       type: String,
-      required: [true, 'Phone number is required']
+      required: [true, "Phone number is required"],
     },
     description: {
       type: String,
-      required: [true, 'Description is required']
+      required: [true, "Description is required"],
     },
     imageUrl: {
-      type: String
+      type: String,
     },
+    logoUrl: String,
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
- 
-const Restaurant = model('Restaurant', restaurantSchema);
+
+const Restaurant = model("Restaurant", restaurantSchema);
 
 module.exports = Restaurant;
