511f047f7d7a3170dbd445b13a57e76230cff6c1
Alberto Valenzuela <avalen28@gmail.com>
2023-02-16 19:11:10 +0100

    💄✨📝 corrects and implement img and logo default of restaurants. adds correct width in signup options

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -34,12 +34,18 @@ const restaurantSchema = new Schema(
     },
     imageUrl: {
       type: String,
+      default:
+        "https://scontent.fbcn3-1.fna.fbcdn.net/v/t39.30808-6/243200278_260965582698292_6813909715328889748_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=LPQr9b8_V9gAX8-sLtB&_nc_ht=scontent.fbcn3-1.fna&oh=00_AfCb2oIdnDxe5YX8vG6MdTC8dmuuBJ0GkvXGRr7SE9q98Q&oe=63F3D21D",
     },
-    logoUrl: String,
+    logoUrl: {
+      type: String,
+      default:
+        "https://img.freepik.com/vector-gratis/ejemplo-lindo-icono-vector-historieta-salmon-sushi-concepto-icono-caracter-comida-estilo-dibujos-animados-plana_138676-2590.jpg",
+    },
     status: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   {
     timestamps: true,
